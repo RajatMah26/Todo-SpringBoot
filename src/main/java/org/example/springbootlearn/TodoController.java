869e5b2d75c4f86202a4f77cc8e5afc374d17d92
Rajatdhoot26 <2rajatd6@gmail.com>
2025-01-09 13:38:00 +0530

    ResopenEntity for staus

--- src/main/java/org/example/springbootlearn/TodoController.java
+++ src/main/java/org/example/springbootlearn/TodoController.java
@@ -1,10 +1,9 @@
 package org.example.springbootlearn;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -20,14 +19,18 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return todosList;
+    public ResponseEntity<List<Todo>> getTodos(){
+       // return ResponseEntity.status(HttpStatus.OK).body(todosList);
+        return ResponseEntity.ok(todosList);
     }
 
+
+    // @ResponseStatus(HttpStatus.CREATED) we can use for status annotaions to update
+
     @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo){
+    public ResponseEntity<Todo>  createTodo(@RequestBody Todo newTodo){
             todosList.add(newTodo);
-            return newTodo;
+            return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
 
