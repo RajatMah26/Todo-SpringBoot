b1b09aebf599d0e434650f2e7f7a9fef0ea14471
Rajatdhoot26 <2rajatd6@gmail.com>
2025-01-09 14:53:17 +0530

    Not Found print ResponeEntity

--- src/main/java/org/example/springbootlearn/TodoController.java
+++ src/main/java/org/example/springbootlearn/TodoController.java
@@ -11,43 +11,39 @@ import java.util.*;
 @RequestMapping("/api/v1/todos")
 public class TodoController {
 
-    private  static List<Todo> todosList;
+    private static List<Todo> todosList;
 
-    public TodoController(){
-        todosList=new ArrayList<>();
+    private static final String TODO_NOT_FOUND = "Todo not found";
+
+    public TodoController() {
+        todosList = new ArrayList<>();
         todosList.add(new Todo(1,false,"Todo1",1));
         todosList.add(new Todo(2,true,"Todo1",2));
     }
 
     @GetMapping
     public ResponseEntity<List<Todo>> getTodos(){
-       // return ResponseEntity.status(HttpStatus.OK).body(todosList);
+        // return ResponseEntity.status(HttpStatus.OK).body(todosList);
         return ResponseEntity.ok(todosList);
     }
 
-
-    // @ResponseStatus(HttpStatus.CREATED) we can use for status annotaions to update
-
     @PostMapping
     public ResponseEntity<Todo>  createTodo(@RequestBody Todo newTodo){
-            todosList.add(newTodo);
-            return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+        todosList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<Todo> getTodoById(@PathVariable Long todoId){
-        for(Todo todo :todosList){
-            if(todo.getId()==todoId){
+    public ResponseEntity<?> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todosList) {
+            if (todo.getId() == todoId) {
                 return ResponseEntity.ok(todo);
             }
         }
 
-       return ResponseEntity.notFound().build();
-        //return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        // return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
     }
-
-
-
 
 }
